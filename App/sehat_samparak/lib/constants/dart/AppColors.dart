d94160b37e2c0873e0ec989c19c006fe79f3cdba
vidit.khandelwal <vidit.khandelwal@radixweb.com>
2023-06-19 17:48:24 +0530

    Created the design of Login screen.

---
+++ App/sehat_samparak/lib/constants/dart/AppColors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF503E9D);
+  static const Color primaryColorLight = Color(0xFF6252A7);
+}
--- App/sehat_samparak/lib/screens/login.dart
+++ App/sehat_samparak/lib/screens/login.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../constants/dart/AppColors.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,8 +11,160 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController phoneController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      key: const ValueKey(1),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 20),
+                        child: Stack(
+                          children: [
+                            Center(
+                              child: Container(
+                                height: 240,
+                                constraints: const BoxConstraints(maxWidth: 500),
+                                margin: const EdgeInsets.only(top: 100),
+                                decoration: const BoxDecoration(
+                                  color: Color(0xFFE1E0F5),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(30)),
+                                ),
+                              ),
+                            ),
+                            Center(
+                              child: Container(
+                                constraints: const BoxConstraints(maxHeight: 340),
+                                margin: const EdgeInsets.symmetric(horizontal: 8),
+                                child: Image.asset('assets/img/login.png'),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 10),
+                        child: const Text(
+                          'Sehat Samparak',
+                          style: TextStyle(
+                              color: AppColors.primaryColor,
+                              fontSize: 30,
+                              fontWeight: FontWeight.w800),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Column(
+                    children: [
+                      Container(
+                          constraints: const BoxConstraints(maxWidth: 500),
+                          margin: const EdgeInsets.symmetric(horizontal: 10),
+                          child: RichText(
+                            textAlign: TextAlign.center,
+                            text: const TextSpan(children: [
+                              TextSpan(
+                                  text: 'We will send you an ',
+                                  style:
+                                      TextStyle(color: AppColors.primaryColor)),
+                              TextSpan(
+                                  text: 'One time password ',
+                                  style: TextStyle(
+                                      color: AppColors.primaryColor,
+                                      fontWeight: FontWeight.bold)),
+                              TextSpan(
+                                  text: 'on this mobile number',
+                                  style:
+                                      TextStyle(color: AppColors.primaryColor)),
+                            ]),
+                          )),
+                      Container(
+                        height: 40,
+                        constraints: const BoxConstraints(maxWidth: 500),
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 10),
+                        child: CupertinoTextField(
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          decoration: const BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.all(Radius.circular(4))),
+                          controller: phoneController,
+                          clearButtonMode: OverlayVisibilityMode.editing,
+                          keyboardType: TextInputType.phone,
+                          maxLines: 1,
+                          placeholder: '+91...',
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 10),
+                        constraints: const BoxConstraints(maxWidth: 500),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              primary: AppColors.primaryColor),
+                          onPressed: () {
+                            if (phoneController.text.isNotEmpty) {
+                              print('Get OTP');
+                            } else {
+                              const SnackBar(
+                                behavior: SnackBarBehavior.floating,
+                                backgroundColor: Colors.red,
+                                content: Text(
+                                  'Please enter a valid mobile number',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              );
+                            }
+                          },
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 8, horizontal: 8),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text(
+                                  'Next',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.all(8),
+                                  decoration: const BoxDecoration(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(20)),
+                                    color: AppColors.primaryColorLight,
+                                  ),
+                                  child: const Icon(
+                                    Icons.arrow_forward_ios,
+                                    color: Colors.white,
+                                    size: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
